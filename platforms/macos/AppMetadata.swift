b80e8016fae5c951d92133cd9d347e66b389fffc
Whoosh <nhatkha1407@gmail.com>
2025-12-08 21:23:31 +0700

    Update AppMetadata.swift

--- platforms/macos/AppMetadata.swift
+++ platforms/macos/AppMetadata.swift
@@ -66,7 +66,6 @@ enum AppMetadata {
 enum SettingsKey {
     static let enabled = "gonhanh.enabled"
     static let method = "gonhanh.method"
-    static let modernTone = "gonhanh.modernTone"
     static let hasCompletedOnboarding = "gonhanh.onboarding.completed"
     static let permissionGranted = "gonhanh.permission.granted"
 }
